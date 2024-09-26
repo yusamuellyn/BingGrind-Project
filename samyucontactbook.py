f94c88eb203125fb5e54ad38a66796fda3ee82e9
Samuel <syu12@binghamton.edu>
2024-09-26 15:20:50 -0400

    commit

--- samyucontactbook.py
+++ samyucontactbook.py
@@ -1,55 +1,61 @@
 def main(): 
-    contactlist = open("contactinfo.txt", "w+")
-    choice(contactlist)
+    choice()
     while True:
         decision = str(input("Do you want to do anything else? Yes or No"))
         if decision == "Yes": 
-            choice(contactlist)
-        else:
-            contactlist.close()
+            choice()
+        elif decision == "No":
             break
+        else:
+            "Invalid Answer"
 
    
-def choice(contactlist):
+def choice():
     decision = str(input("Do you want to add, delete, or search contact info? "))
     if decision == "add":
-        addContact(contactlist)
+        addContact()
     elif decision == "delete":
-        deleteContact(contactlist)
+        deleteContact()
     elif decision == "search":
         searchContact()
     else:
         print("Invalid answer")
     
-def addContact(contactlist):
+def addContact():
     add = str(input("What is the name of your contact? "))
-    #addnumber = str(input("What is the phone number of your contact? "))
-    contactlist.writelines([add + "\n"])
-    #contactlist.writelines([addnumber + "\n"])
+    addnumber = str(input("What is the phone number of your contact? "))
+    with open("contactinfo.txt", "a") as file:
+        file.write(add + " " + addnumber +  "\n")
     
-    
-    
-def deleteContact(contactlist):
+def deleteContact():
     delete = str(input("What contact do you want to delete? "))
-    contactlist.seek(0)
-    num_lines = contactlist.readline()
-    found = False
-    with open("contactinfo.txt", "w") as temp_file:
-        for lines in num_lines:
-            if num_lines != delete:
-                temp_file.write(lines)
-            else:
-                found = True
-        
-    if found == True: 
-        print("Deleted contact")
-    else:
-        print("Contact not found")
-            
+    deleteNum = str(input("What is the phone number of the contact? "))
+    with open("contactinfo.txt", "r") as file:
+            lines = file.readlines()
+   
+    with open("contactinfo.txt", "w") as file:
+            for line in lines:
+                if line.__contains__(delete) == False:
+                    file.write(line)
+                else:
+                    print(delete + " has been deleted")
+                    
+    with open('contactinfo.txt', 'r') as file:
+        for line in file:
+            print(line.strip())
+    
                         
 def searchContact():
-    search = str(input("What contact do you want to search for"))
-    
-        
+    search = str(input("What contact do you want to search for? "))
+    names = []
+    with open("contactinfo.txt", "r") as file:
+        num = 0
+        for line in file:
+            if line.__contains__(search):
+                num = num + 1
+                print(line.strip())
+    if num == 0:
+        print("Contact not found")
+                  
 if __name__ == "__main__":
     main()
