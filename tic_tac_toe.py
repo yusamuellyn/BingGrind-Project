6bc9cfaee2044c04c4491e209db46fd7198d1ccd
Samuel <syu12@binghamton.edu>
2024-10-09 16:43:09 -0400

    commit

---
+++ tic_tac_toe.py
@@ -0,0 +1,14 @@
+import tkinter as tk 
+
+def main():
+    board()
+    
+def board():
+    tic_board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+    print(tic_board)
+if __name__  == "__main__":
+    main()
